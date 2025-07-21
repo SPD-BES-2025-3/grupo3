ee8a344a7e479528b61a8f069a8afa45e3a7a97e
MauricioMoraesP <mmprogrammer@hotmail.com>
2025-07-21 16:02:42 -0300

    Projeto parte 2 com ODM

---
+++ medical-prototipo2/src/test/java/com/odm/MedicalPrototipo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalPrototipo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
