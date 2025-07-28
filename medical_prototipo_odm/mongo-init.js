3f86d8a05af086a5755c5ea066458a9f715e94cb
Matheus Queiroz <71334913+mqrb01@users.noreply.github.com>
2025-07-27 22:38:01 -0300

    Código para a criação do Banco de Dados

---
+++ medical_prototipo_odm/mongo-init.js
@@ -0,0 +1,6 @@
+// Script de inicialização do MongoDB
+// Este script é executado quando o container do MongoDB é criado pela primeira vez
+
+// Conecta ao banco de dados medical_db
+db = db.getSiblingDB('medical_db');
+
