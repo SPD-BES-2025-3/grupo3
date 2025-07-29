28b22d87a163b8764c156fe05d416754decdc0bb
Matheus Queiroz <71334913+mqrb01@users.noreply.github.com>
2025-07-29 00:17:27 -0300

    Update config.py

--- medical_prototipo_odm/src/config.py
+++ medical_prototipo_odm/src/config.py
@@ -1,8 +1,8 @@
+import os
 from mongoengine import connect
 
 def init_db():
-    connect(
-        db="mongodb",
-        host="localhost",
-        port=27017
-    )
+    mongo_uri = os.getenv("MONGODB_URI", "mongodb://admin:1234@localhost:27017/medical_db?authSource=admin")
+    connect(host=mongo_uri)
+
+   
