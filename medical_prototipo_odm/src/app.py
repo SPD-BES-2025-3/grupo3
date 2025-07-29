c5579bc8bf2fd3e060c12cf78f42021e8e0f419d
Matheus Queiroz <71334913+mqrb01@users.noreply.github.com>
2025-07-29 00:17:48 -0300

    Update app.py

--- medical_prototipo_odm/src/app.py
+++ medical_prototipo_odm/src/app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask_cors import CORS 
 from config import init_db 
 from controllers.consulta_controller import consulta_bp
 from controllers.prontuario_controller import prontuario_bp
 
 app = Flask(__name__)
+CORS(app) 
 init_db()
 
 app.register_blueprint(consulta_bp)
 app.register_blueprint(prontuario_bp)
 
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
